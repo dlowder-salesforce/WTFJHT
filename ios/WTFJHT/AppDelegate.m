449fdc3e4e9532a4a234701c9a2aade6606c98ee
dlowder-salesforce <dlowder@salesforce.com>
2017-10-03 10:06:46 -0700

    Project changes for local device building

--- ios/WTFJHT/AppDelegate.m
+++ ios/WTFJHT/AppDelegate.m
@@ -18,7 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios"
+                                                                  fallbackResource:@"index.ios"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"WTFJHT"
